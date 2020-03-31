fd5c368127d3234091adf908661161652b5c7a0e
mt-0509 <ppsr5504@yahoo.co.jp>
2020-03-31 17:55:51 +0900

    Object.createについて学んだが、普通にオブジェクトを作成するのと 何が違うのか理解できなかった

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 'use strict';
 
 {
-  
+  // 自分
+  var nob = Object.create(null);
+  nob = {p:1}
+  console.log(nob);
+
+  // 回答
+  const obj = Object.create({}, {p: {value: 1}});
+  console.log(obj.p);
 
 }

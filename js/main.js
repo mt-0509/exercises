b6be36f46aae988d148a59514eea6c39058c7200
mt-0509 <ppsr5504@yahoo.co.jp>
2020-04-02 15:16:41 +0900

    マウスホバーの作り方を学んだ

--- js/main.js
+++ js/main.js
@@ -1,24 +1,40 @@
 'use strict';
 
 {
+  // クリックで色を変更する
   let one = document.getElementById('one');
-
+  
   one.addEventListener('click', () => {
     one.style.backgroundColor = 'skyblue';
   })
 
+  // クリックでカウントアップする
   let two = document.getElementById('two');
-
-  function countUp() {
-    document.getElementById('two').innerText = ++count;
-  }
   let count = 0;
 
+  function countUp() {
+    document.getElementById('two').querySelector('p').innerHTML = ++count;
+  }
+
   two.addEventListener('click', () => {
-    
     countUp();
   })
   
+
+  // マウスホバー
+  let three = document.getElementById('three');
+  
+  // マウスが触れたとき
+  three.addEventListener('mouseover', () => {
+    three.style.backgroundColor = 'orange';
+  }, false)
+
+  // マウスが離れたとき
+  three.addEventListener('mouseleave', () => {
+    three.style.backgroundColor = 'pink';
+  }, false)
+
+
   
 
 }

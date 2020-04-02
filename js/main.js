013a98eff7f58eeaadec1a7ffd7128cc0ef32dda
mt-0509 <ppsr5504@yahoo.co.jp>
2020-04-02 17:27:00 +0900

    プロトタイプについて学んだ プロトタイプを使ってメソッドを作成する場合参照となる

--- js/main.js
+++ js/main.js
@@ -1,41 +1,19 @@
 'use strict';
 
 {
-  // クリックで色を変更する
-  let one = document.getElementById('one');
-  
-  one.addEventListener('click', () => {
-    one.style.backgroundColor = 'skyblue';
-  })
+  function Who(name) {
+    this.name = name;
+  };
+  Who.prototype.getName = function() {
+    console.log('Myname is ' + this.name);
+  };
+  var o = new Who('morita');
+  o.getName();
 
-  // クリックでカウントアップする
-  let two = document.getElementById('two');
-  let count = 0;
-
-  function countUp() {
-    document.getElementById('two').querySelector('p').innerHTML = ++count;
-  }
-
-  two.addEventListener('click', () => {
-    countUp();
-  })
-  
-
-  // マウスホバー
-  let three = document.getElementById('three');
-  
-  // マウスが触れたとき
-  three.addEventListener('mouseover', () => {
-    three.style.backgroundColor = 'orange';
-  }, false)
-
-  // マウスが離れたとき
-  three.addEventListener('mouseleave', () => {
-    three.style.backgroundColor = 'pink';
-  }, false)
-
-
-  
 
 }
+
+
+
+
 

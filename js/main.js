50c4bdf76f8bbdcb23840abc680100f05f80b78f
mt-0509 <ppsr5504@yahoo.co.jp>
2020-04-03 16:43:15 +0900

    チェックボックスの扱い方を学んだ アロー関数の中にはfor文を書けない理由が不明

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 'use strict';
 
 {
-  function Who(name) {
-    this.name = name;
-  };
-  Who.prototype.getName = function() {
-    console.log('Myname is ' + this.name);
-  };
-  var o = new Who('morita');
-  o.getName();
 
+  document.getElementById('ch').addEventListener('click', () => {
 
+  })
+
+  
 }
 
 

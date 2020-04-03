5fd70da7f63bd125eab1fcef3660c30d52e81f8b
mt-0509 <ppsr5504@yahoo.co.jp>
2020-04-03 17:14:49 +0900

    jsファイルを外部から読み込む時にはfor文内のiにletをつけないとエラーになる

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
 'use strict';
 
 {
-
+  
   document.getElementById('ch').addEventListener('click', () => {
-
+    function chbox() {
+      var str = "";
+  
+      for(let i = 0; i < 6; i++) {
+        if(document.chbox.elements[i].checked) {
+          if(str != "") str = str + ",";
+  
+          str = str + document.chbox.elements[i].value;
+        }
+      }
+  
+      if(str == "") {
+        alert("どれか選択してください。");
+      } else {
+        alert(str + "が選択されました。");
+      }
+    }
+    
+    chbox();
   })
 
+      
   
 }
 

0e9f2e58df26def79a9cb2875dd1b7fae496c80b
mt-0509 <ppsr5504@yahoo.co.jp>
2020-04-05 12:53:36 +0900

    セレクトボックスの扱い方を学んだ select.optionsでselect要素に含まれるoption要素のHTMLOptionsCollection を返す

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
 'use strict';
 
 {
+  document.getElementById('change').addEventListener('change', () => {
+        // セ・リーグのチームの配列
+        let s_league = [
+          "", "中日", "ヤクルト", "巨人", "阪神", "横浜", "広島",
+        ];
+        let p_league = [
+          "", "ロッテ", "西武", "オリックス", "ソフトバンク", "楽天", "日本ハム" 
+        ];
+        
+    
+        function teamSet() {
+          for (let i = 1; i < 7; i++) {
+            switch(document.selbox.league.selectedIndex) {
+              case 0: document.selbox.team.options[i].text = "";break;
+              case 1: document.selbox.team.options[i].text = s_league[i];break;
+              case 2: document.selbox.team.options[i].text = p_league[i];break;
+            }
+          }
+    
+          document.selbox.team.selectedIndex = 0;
+        }
+        teamSet();
+        
+  })
 
 
       

18be333391fccc019b31a0873e8960df6fb3a11b
mt-0509 <ppsr5504@yahoo.co.jp>
2020-04-07 02:45:34 +0900

    sort()で比較関数を使って配列を並び替える方法

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 'use strict';
 
 {
-  
+  let array = ['e', 'a', 'k', 'B', 'c'];
+
+  // 比較関数
+  function compareFunc(a,b) {
+    if(a.toUpperCase() > b.toUpperCase()) {
+      return 1;
+    } else {
+      return -1;
+    }
+  }
+
+  array.sort(compareFunc);
+  console.log(array);
       
   
 }

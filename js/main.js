df182f38508673892d63d6f1cdef61f123cd9de8
mt-0509 <ppsr5504@yahoo.co.jp>
2020-04-12 12:11:44 +0900

    検索した文字を置換する方法を学んだ

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
 'use strict';
 
 {
-  // 文字であることを確認した
-  let a = '10';
-  let kata =  typeof a;
-  console.log(kata);
+  let str
+ = "文字を置換するには、##replace()%%を使います。"
+ + "括弧内の最初に##検索文字列%%、2番目に##置換文字列%%を記入します。"
+ + "結果は、##最初にHitした文字列%%が置換された状態で返されます。";
 
-  // 数字ではないことを確認した
-  let check = isNaN(a);
-  console.log(check);
+ // 文字を置換する
+ str = str.replace(/##/g, "<strong>");
+ str = str.replace(/%%/g, "<strong>");
 
-  // 文字を数字に変換した
-  let result = a.toString();
-  console.log(result);
+ document.write(str);
+  
 }
 
 

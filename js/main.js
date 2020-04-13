6dcf257c78754fd929cf048981ac6635c4c198c9
mt-0509 <ppsr5504@yahoo.co.jp>
2020-04-13 11:43:30 +0900

    setInterval()とsubstring()の使い方を学んだ

--- js/main.js
+++ js/main.js
@@ -1,17 +1,7 @@
 'use strict';
 
 {
-  function add(x, y){
-    if(typeof y == 'undefined') { //部分適用
-      return function(y) {
-        return x + y
-      }
-    }
-    // 完全適用
-    return x + y
-  }
-  add(1)(2) // 3
-  add(1, 2) // 3
+  
 }
 
 

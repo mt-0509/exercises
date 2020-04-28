47cd8fcb9bc7af38dba22bd7213b6ded73ce7fa7
mt-0509 <ppsr5504@yahoo.co.jp>
2020-04-28 22:14:17 +0900

    vue.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
-'use strict';
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: 'Vue.js練習問題'
+  }
+})
 
-{
-  let fafa = Factory('morita');
-  console.log(fafa.introduce());
-}
+var app2 = new Vue({
+  el: "#app2",
+  data: {
+    count: 0
+  },
+  methods: {
+    ganba: function() {
+      alert('こんにちは');
+    },
+    count_up: function () {
+      this.count++;
+    }
+  }
+})
 
-
-
-
-
+var app3 = new Vue({
+  el: "#app3",
+  data: {
+    count: 0
+  },
+  methods: {
+    count_up: function () {
+      this.count++;
+    }
+  }
+})

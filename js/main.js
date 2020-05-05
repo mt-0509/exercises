fb58260c7458dbd3f1c95e03f20d0ab34b36cf1e
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-05 21:35:41 +0900

    jsのみでリアクティブ

--- js/main.js
+++ js/main.js
@@ -1,49 +1,22 @@
-new Vue({
-  el: "#app",
-  data: {
-    students: [
-      {name: '田中太朗', sex:'男性', age:'24'},
-      {name: '田中次郎', sex:'男性', age:'19'},
-      {name: '田中花子', sex:'女性', age:'14'},
-    ],
-    name: '',
-    sex: '',
-    age: ''
-  },
-  methods: {
-    push: function () {
-      this.students.push({
-        // name: this.name,
-        // sex: this.sex,
-        // age: this.age, 
+'use strict';
+{
+  // inputタグを取得
+  let write = document.getElementById('write');
+  
+  // 入力値に変更があった際にイベント発動
+  write.addEventListener('change', () => {
+    setInterval(change,1)
+      if (write.value !== '') {
+        const comment = document.getElementById('comment');
+        comment.classList.add('delete');
+      } else {
+        comment.classList.remove('delete');
+      }
+  })
 
-        name: one.value,
-        sex: two.value,
-        age: three.value, 
-      });
-      
-      this.name = "",
-      this.sex = "",
-      this.age = ""
-    },
-    update: function (index) {
-      // this.$refs.r1.value = this.students[index].name
-      // this.$refs.r2.value = this.students[index].age
-      // this.$refs.r3.value = this.students[index].sex
-
-      let one = document.getElementById('one');
-      let two = document.getElementById('two');
-      let three = document.getElementById('three');
-
-      one.value = this.students[index].name;
-      two.value = this.students[index].age;
-      three.value = this.students[index].sex;
-      
-      
-      
-    },
-    sakujo: function (index) {
-      this.students.splice(index, 1);
-    }
+  // 入力値を反映する処理
+  const change = () => {
+    let sentence = document.getElementById('sentence');
+    sentence.innerHTML = write.value;  
   }
-})
+}

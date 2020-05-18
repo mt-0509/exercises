047fe8348b96e680475824840e577c228809aae1
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-18 22:22:22 +0900

    との使い方

--- js/main.js
+++ js/main.js
@@ -3,39 +3,19 @@
   new Vue ({
   el:'#app',
   data: {
-    name: 'キマイラ',
-    list: [
-      {id: 1, name: 'スライム', hp: 100},
-      {id: 2, name: 'ゴブリン', hp: 200},
-      {id: 3, name: 'ドラゴン', hp: 500}
-    ]
+    show: true
   },
   methods: {
-    doAdd: function () {
-      let max = this.list.reduce(function (a, b) {
-        return a > b.id ? a : b.id
-      }, 0)
-      this.list.push({
-        id: max + 1,
-        name: this.name,
-        hp: 500
-      })
-    },
-    doRemove: function (index) {
-      this.list.splice(index, 1)
-    },
-    created: function () {
-      this.list.forEach(function(item) {
-        this.$set(item, 'active',false)
-      },this)
+    handleClick() {
+      let count = this.$refs.count
+      if(count) {
+        count.innerText = parseInt(count.innerText, 10) + 1
+      }
     }
   }
+  
+  
 })
-
-this.list[0] = {id: 1, name: 'キングスライム', hp: 500}
-
-// this.$set(更新するデータ, インデックス or キー, 新しい値)
-this.$set(this.list, 0, {id: 1, name: 'キングスライム', hp: 500})
 
 
 

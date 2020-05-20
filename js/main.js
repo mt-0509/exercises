5a6248ba6872f0a38d26f5f78086ffc2110effd7
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-21 00:07:22 +0900

    算出プロパティの挙動

--- js/main.js
+++ js/main.js
@@ -4,21 +4,12 @@
     el: '#app',
     data: {
       width: 800,
-      height: 600
       },
     computed: {
-      halfWidth: function () {
-        return this.width / 2
-      },
-      halfHeight: function () {
-        return this.height / 2
-      },
-      halfPoint: function () {
-        return {
-          x: this.halfWidth,
-          y: this.halfHeight
-        }
-      }
+      computedData: function () { return Math.random()}
+    },
+    methods: {
+      methodsData: function () { return Math.random()}
     }
   })
 }

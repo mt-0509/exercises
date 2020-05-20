8098c256484b356c66cf48607c7ee07896ae8231
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-20 22:48:28 +0900

    算出プロパティの組み合わせ

--- js/main.js
+++ js/main.js
@@ -3,26 +3,24 @@
   new Vue({
     el: '#app',
     data: {
-      width: 800
+      width: 800,
+      height: 600
       },
-
     computed: {
       halfWidth: function () {
         return this.width / 2
       },
-      x: function () {
-
-        console.log(this.halfWidth);
-
+      halfHeight: function () {
+        return this.height / 2
+      },
+      halfPoint: function () {
+        return {
+          x: this.halfWidth,
+          y: this.halfHeight
+        }
       }
     }
   })
-  
-  
-  
-  
-  
-  
 }
 
 

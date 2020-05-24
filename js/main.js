a1179b7d299da119bbcbb178a2e8a1a7613b3516
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-24 16:17:45 +0900

    vue.js watch

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
 'use strict';
 {
-  new Vue({
+  var vm = new Vue({
     el: '#app',
     data: {
-      width: 800,
-      },
-    computed: {
-      computedData: function () { return Math.random()}
+        firstName: '',
+        lastName: '',
+        // プロパティはあらかじめセットしておく
+        fullName: '',
+        now: '',
     },
-    methods: {
-      methodsData: function () { return Math.random()}
+    watch: {
+        // firstName/lastName/now: 監視対象のプロパティ名
+        // newValue: 更新後のプロパティの値
+        // oldValue: 更新前のプロパティの値
+        firstName: function (newValue, oldValue) {
+            console.log(newValue, oldValue);
+            this.fullName = newValue + ' ' + this.lastName
+        },
+        lastName: function (newValue, oldValue) {
+            console.log(newValue, oldValue);
+            this.fullName = this.firstName + ' ' + newValue
+        },
+        // 監視しているが、値に変化がないので変更されることはない
+        now: function () {
+            return Date.now()
+        }
     }
-  })
+})
+
 }
 
 

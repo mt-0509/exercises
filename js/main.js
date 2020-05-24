bff5d74bed684cc7c405613ec0b2625d39e678d2
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-25 00:59:09 +0900

    フィルタの使い方

--- js/main.js
+++ js/main.js
@@ -1,37 +1,16 @@
 'use strict';
 {
-  var vm = new Vue({
+  new Vue({
     el: '#app',
-    data: {
-        firstName: '',
-        lastName: '',
-        // プロパティはあらかじめセットしておく
-        fullName: '',
-        now: '',
-    },
-    watch: {
-        // firstName/lastName/now: 監視対象のプロパティ名
-        // newValue: 更新後のプロパティの値
-        // oldValue: 更新前のプロパティの値
-        firstName: function (newValue, oldValue) {
-            console.log(newValue, oldValue);
-            this.fullName = newValue + ' ' + this.lastName
-        },
-        lastName: function (newValue, oldValue) {
-            console.log(newValue, oldValue);
-            this.fullName = this.firstName + ' ' + newValue
-        },
-        // 監視しているが、値に変化がないので変更されることはない
-        now: function () {
-            return Date.now()
-        }
+    filters: {
+      round: function(val) {
+        return Math.round(val * 100) / 100
+      },
+      radian: function(val) {
+        return val * Math.PI / 180
+      }
     }
-})
+  })
+
 
 }
-
-
-
-
-
-

896e18e3b0fdfc1bcb64cbac85cdda0586cbf2dd
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-31 13:08:13 +0900

    トランジション

--- js/main.js
+++ js/main.js
@@ -1,23 +1,10 @@
 'use strict';
 {
-// メッセージ一覧用コンポーネント
-Vue.component('comp-board', {
-  template: '<div>Message Board</div>',
-})
-// 入力フォーム用コンポーネント
-Vue.component('comp-form', {
-  template: '<div>Form<textarea v-model="message"></textarea></div>',
-  data: function () {
-    return {
-      message: ''
-    }
-  }
-})
 
 new Vue({
   el: '#app',
   data: {
-    current: 'comp-board' // 動的に切り替える
+    show: true
   }
 })
 

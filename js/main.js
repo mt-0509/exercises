1a879bc168edeeadfabdc261d52066ea3dcba8df
mt-0509 <ppsr5504@yahoo.co.jp>
2020-06-01 13:36:27 +0900

    リストトランジション

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 'use strict';
 {
+  new Vue({
+    el: '#app',
+    data: {
+      order: false,
+      list: [
+        { id: 1, name: 'りんご', price: 100 },
+        { id: 2, name: 'ばなな', price: 200 },
+        { id: 3, name: 'いちご', price: 300 }
+      ]
+    },
+    computed: {
+      // orderの値でリストの順番を反転する算出プロパティ
+      sortedList: function () {
+        // LodashのorderByメソッドを使用
+        return _.orderBy(this.list, 'price', this.order ? 'desc' : 'asc')
+      }
+    }
+  })
 
-new Vue({
-  el: '#app',
-  data: {
-    show: true
-  }
-})
 
 
 }

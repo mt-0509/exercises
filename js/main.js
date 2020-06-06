9897afad0e0d9b811b55ac84baa33cfb5ec63313
mt-0509 <ppsr5504@yahoo.co.jp>
2020-06-06 19:05:53 +0900

    ToDoリスト作成

--- js/main.js
+++ js/main.js
@@ -1,24 +1,80 @@
 'use strict';
 {
-  new Vue({
+  var STORAGE_KEY = 'todos-vuejs-demo'
+  var todoStorage = {
+    fetch: function() {
+      var todos = JSON.parse(
+        localStorage.getItem(STORAGE_KEY) || '[]'
+      )
+      todos.forEach(function(todo, index) {
+        todo.id = index
+      })
+      todoStorage.uid = todos.length
+      return todos
+    },
+    save: function(todos) {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+    }
+  }
+
+
+
+  const app = new Vue({
     el: '#app',
     data: {
-      order: false,
-      list: [
-        { id: 1, name: 'りんご', price: 100 },
-        { id: 2, name: 'ばなな', price: 200 },
-        { id: 3, name: 'いちご', price: 300 }
-      ]
+      todos: [],
+      options: [
+        {value: -1, label: 'すべて'},
+        {value: 0, label: '作業中'},
+        {value: 1, label: '完了'},
+      ],
+      current: -1
+    },
+    watch: {
+      todos: {
+        handler: function(todos) {
+          todoStorage.save(todos)
+        },
+        deep: true
+      }
+    },
+    created() {
+      this.todos = todoStorage.fetch()
+    },
+    methods: {
+      doAdd: function(event, value) {
+        var comment = this.$refs.comment
+        if(!comment.value.length) {
+          return
+        }
+        this.todos.push({
+          id: todoStorage.uid++,
+          comment: comment.value,
+          state: 0
+        })
+        comment.value = ''
+      },
+      doChangeState: function(item) {
+        item.state = item.state ? 0 : 1
+      },
+      doRemove: function(item) {
+        var index = this.todos.indexOf(item)
+        this.todos.splice(index, 1)
+      }
     },
     computed: {
-      // orderの値でリストの順番を反転する算出プロパティ
-      sortedList: function () {
-        // LodashのorderByメソッドを使用
-        return _.orderBy(this.list, 'price', this.order ? 'desc' : 'asc')
+      computedTodos: function() {
+        return this.todos.filter(function(el) {
+          return this.current < 0 ? true : this.current === el.state
+        }, this)
+      },
+      labels() {
+        return this.options.reduce(function(a, b) {
+          return Object.assign(a, { [b.value]: b.label})
+        }, {})
       }
     }
   })
-
 
 
 }
